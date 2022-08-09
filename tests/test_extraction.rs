bc53ecc073ad87438da3d522acc3ecbdef7dc099
Robley G <robleyadrian@gmail.com>
2022-08-09 19:10:35 +0300

    add tests

---
+++ tests/test_extraction.rs
@@ -0,0 +1,25 @@
+#[cfg(test)]
+mod tests {
+    const TEST_IMAGE: &str = "./tests/images/test_2.png";
+
+    #[test]
+    fn test_text_extracted() {
+        let img_text = doom_alerts::extract_from_path(TEST_IMAGE);
+        assert!(img_text.starts_with("AREA: WHOLE OF UTAWALAFEEDER, DATE: Tuesday 08.03.2022 TIME: 9.00 A.M.—5.00P.M."))
+    }
+
+    #[test]
+    fn test_regex_extraction() {
+        let img_text = doom_alerts::extract_from_path(TEST_IMAGE);
+        let outages = doom_alerts::parse_text(&img_text);
+        assert_eq!(outages.len(), 3);
+    }
+
+    #[test]
+    fn test_location_in_text() {
+        let img_text = doom_alerts::extract_from_path(TEST_IMAGE);
+        let first_outage = &doom_alerts::parse_text(&img_text)[0];
+
+        assert!(first_outage.includes_location("Parts of Eastern Bypass"));
+    }
+}
