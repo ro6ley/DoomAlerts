1d56bae618e900d894beef074c37bfe4e9801389
Robley G <robleyadrian@gmail.com>
2022-08-14 19:55:39 +0300

    updated main

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,13 @@ async fn main() {
     let api_key: String = env::var("API_KEY").expect("$API_KEY env var is not set");
     let api_key_secret: String =
         env::var("API_KEY_SECRET").expect("$API_KEY_SECRET env var is not set");
+    let locations: String = env::var("LOCATIONS").expect("$LOCATIONS env var is not set");
 
     let con_token: KeyPair = KeyPair::new(api_key, api_key_secret);
-    // TODO: Access variant of the Token enum instead of the Bearer variant.
     let token: Token = auth::bearer_token(&con_token).await.unwrap();
 
-    doom_alerts::fetch_tweets(token, "KenyaPower_care").await;
+    let outage_texts: Vec<String> = doom_alerts::fetch_tweets(token, "KenyaPower_care").await;
+
+    // TODO: Send notification if affected == true
+    let _affected: bool = doom_alerts::search(outage_texts, locations).unwrap();
 }
