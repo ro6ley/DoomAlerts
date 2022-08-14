0006c925e65c535b23011f97878582ba26df4043
Robley G <robleyadrian@gmail.com>
2022-08-14 19:58:26 +0300

    add search functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,81 +1,19 @@
 use egg_mode::{tweet::user_timeline, tweet::Timeline, user::UserID, Token};
 use leptess::LepTess;
-use regex::Regex;
 use std::borrow::Cow::Borrowed;
-use std::fmt::{Display, Formatter, Result};
+use tantivy::{
+    collector::TopDocs, doc, query::QueryParser, schema::*, Index, IndexWriter, ReloadPolicy,
+};
 
-#[derive(Debug, Default)]
-pub struct Outage {
-    region: Option<String>,
-    county: Option<String>,
-    area: Option<String>,
-    date: Option<String>,
-    start_time: Option<String>,
-    end_time: Option<String>,
-    locations: Option<String>,
-}
+pub mod outages;
 
-impl Display for Outage {
-    fn fmt(&self, f: &mut Formatter) -> Result {
-        writeln!(
-            f,
-            "REGION: {}",
-            self.region
-                .as_ref()
-                .unwrap_or(&"No region details found.".to_string())
-        )?;
-        writeln!(
-            f,
-            "COUNTY: {}",
-            self.county
-                .as_ref()
-                .unwrap_or(&"No county details found.".to_string())
-        )?;
-        writeln!(
-            f,
-            "AREA: {}",
-            self.area
-                .as_ref()
-                .unwrap_or(&"No area details found.".to_string())
-        )?;
-        writeln!(
-            f,
-            "DATE: {} ({} - {})",
-            self.date
-                .as_ref()
-                .unwrap_or(&"No date details found.".to_string()),
-            self.start_time.as_ref().unwrap_or(&"N/A".to_string()),
-            self.end_time.as_ref().unwrap_or(&"N/A".to_string())
-        )?;
-        writeln!(
-            f,
-            "LOCATIONS: {}",
-            self.locations
-                .as_ref()
-                .unwrap_or(&"No affected locations details found.".to_string())
-        )?;
-        writeln!(
-            f,
-            "---------------------------------------------------------"
-        )
-    }
-}
-
-impl Outage {
-    pub fn includes_location(&self, location: &str) -> bool {
-        if let Some(l) = &self.locations {
-            return l.contains(location);
-        }
-        false
-    }
-}
-
-pub async fn fetch_tweets(token: Token, username: &'static str) -> () {
+pub async fn fetch_tweets(token: Token, username: &'static str) -> Vec<String> {
     println!("\nFetching {}'s latest tweets... ", username);
 
     let user_id: UserID = UserID::ScreenName(Borrowed(username));
-    // ? filter using keywords
     let timeline: Timeline = user_timeline(user_id, false, false, &token).with_page_size(200);
+
+    let mut outage_texts: Vec<String> = Vec::new();
 
     let (_timeline, feed) = timeline.newer(None).await.unwrap();
     for tweet in &*feed {
@@ -92,8 +30,8 @@ pub async fn fetch_tweets(token: Token, username: &'static str) -> () {
                 for entity in &extended_entities.media {
                     println!("{entity:?}");
 
-                    let img_text: String = fetch_image(&entity.media_url).await.unwrap();
-                    parse_text(&img_text);
+                    let img_text: String = fetch_image_from_url(&entity.media_url).await.unwrap();
+                    outage_texts.push(img_text);
                     println!("\n---------------------------------------------------------\n");
                 }
             }
@@ -102,9 +40,10 @@ pub async fn fetch_tweets(token: Token, username: &'static str) -> () {
             }
         }
     }
+    outage_texts
 }
 
-async fn fetch_image(url: &str) -> Option<String> {
+async fn fetch_image_from_url(url: &str) -> Option<String> {
     let img_bytes = reqwest::get(url).await.unwrap().bytes().await.unwrap();
     Some(extract_from_mem(&img_bytes))
 }
@@ -123,66 +62,56 @@ pub fn extract_from_path(location: &str) -> String {
     lt.get_utf8_text().unwrap()
 }
 
-/// Using regex, extract outage information from the text extracted from image
-pub fn parse_text(text: &str) -> Vec<Outage> {
-    let re: Regex = Regex::new(r"(?mi)^(?P<region>[a-z\s]*\sregion)?\s*((parts\sof)?\b(?P<county>[a-z\s]*\scounty\b))?\s*(\barea:?)\s\b(?P<area>[a-z\s,\.]*)(\bdate:?)\s\b(?P<day>[a-z]*)\b\s*(?P<date>[\d\.]*)\b\s(\btime:?)\s\b(?P<start>[\d\.]*)\b\s\b(?P<start_period>[ap]\.m\.)\s*[-—]\s*\b(?P<end>[\d\.]*)\s*(?P<end_period>[ap]\.m\.)\s*(?P<locations>[a-z0-9&,\s\.]*)\n")
-        .unwrap();
-    let mut outages: Vec<Outage> = Vec::new();
+pub fn search(full_texts: Vec<String>, locations: String) -> tantivy::Result<bool> {
+    // abc,xyz -> abc OR xyz
+    let formatted_locations: String = locations
+        .split(',')
+        .map(|s| format!("\"{}\"", s))
+        .collect::<Vec<String>>()
+        .join(" OR ");
 
-    for captures in re.captures_iter(text) {
-        let mut outage = Outage::default();
+    println!("Searching for {} ...", formatted_locations);
 
-        if let Some(n) = captures.name("region") {
-            outage.region = Some(n.as_str().to_string());
-        }
+    let (index, schema) = build_index(full_texts)?;
+    let text = schema.get_field("text").unwrap();
 
-        if let Some(n) = captures.name("county") {
-            outage.county = Some(n.as_str().to_string());
-        }
+    let reader = index
+        .reader_builder()
+        .reload_policy(ReloadPolicy::OnCommit)
+        .try_into()?;
 
-        if let Some(n) = captures.name("area") {
-            outage.area = Some(n.as_str().to_string());
-        }
+    let searcher = reader.searcher();
+    let query_parser = QueryParser::for_index(&index, vec![text]);
 
-        if let Some(n) = captures.name("day") {
-            if let Some(d) = captures.name("date") {
-                let d_str = format!("{} {}", n.as_str(), d.as_str());
-                outage.date = Some(d_str);
-            } else {
-                outage.date = Some(n.as_str().to_string());
-            }
-        }
+    let query = query_parser.parse_query(&formatted_locations)?;
 
-        if let Some(n) = captures.name("start") {
-            if let Some(t) = captures.name("start_period") {
-                let t_str = format!("{} {}", n.as_str(), t.as_str());
-                outage.start_time = Some(t_str);
-            } else {
-                outage.start_time = Some(n.as_str().to_string());
-            }
-        }
+    let top_docs = searcher.search(&query, &TopDocs::with_limit(10))?;
 
-        if let Some(n) = captures.name("end") {
-            if let Some(t) = captures.name("end_period") {
-                let t_str = format!("{} {}", n.as_str(), t.as_str());
-                outage.end_time = Some(t_str);
-            } else {
-                outage.end_time = Some(n.as_str().to_string());
-            }
-        }
-
-        if let Some(n) = captures.name("locations") {
-            outage.locations = Some(n.as_str().to_string());
-        }
-
-        outages.push(outage);
-    }
-
-    outages
+    Ok(!top_docs.is_empty())
 }
 
-// TODO: check for notices including places within my provided location
-// TODO: API, Yew frontend
+fn build_index(full_texts: Vec<String>) -> tantivy::Result<(Index, Schema)> {
+    let mut schema_builder: SchemaBuilder = Schema::builder();
+    schema_builder.add_text_field("text", TEXT | STORED);
+
+    let schema: Schema = schema_builder.build();
+    let text: Field = schema.get_field("text").unwrap();
+
+    // build index
+    let index: Index = Index::create_in_ram(schema.clone());
+    let mut index_writer: IndexWriter = index.writer(10_000_000)?;
+
+    // populate the index
+    full_texts.iter().for_each(|t: &String| {
+        index_writer
+            .add_document(doc!(
+                text => &**t
+            ))
+            .unwrap();
+    });
+    index_writer.commit()?;
+
+    Ok((index, schema))
+}
+
 // TODO: email, sms notifs, telegram
-// TODO: login via Twitter
-// TODO: Favorite locations
