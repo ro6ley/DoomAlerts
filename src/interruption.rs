1ff5b5eca39c3eeb423417e89b4d1c543db82f34
Robley G <robleyadrian@gmail.com>
2022-08-21 19:23:12 +0300

    rename outage -> interruption

--- src/interruption.rs
+++ src/interruption.rs
@@ -1,8 +1,10 @@
+//! Iinterruption module
+
 use regex::Regex;
 use std::fmt::{Display, Formatter, Result};
 
 #[derive(Debug, Default)]
-pub struct Outage {
+pub struct Interruption {
     region: Option<String>,
     county: Option<String>,
     area: Option<String>,
@@ -12,7 +14,7 @@ pub struct Outage {
     locations: Option<String>,
 }
 
-impl Display for Outage {
+impl Display for Interruption {
     fn fmt(&self, f: &mut Formatter) -> Result {
         writeln!(
             f,
@@ -58,8 +60,10 @@ impl Display for Outage {
     }
 }
 
-impl Outage {
-    pub fn includes_location(&self, location: &str) -> bool {
+impl Interruption {
+    /// Returns a boolean indicating whether the provided location will be
+    /// affected by this power supply interuption
+    pub fn affects_location(&self, location: &str) -> bool {
         if let Some(l) = &self.locations {
             return l.contains(location);
         }
@@ -67,70 +71,71 @@ impl Outage {
     }
 }
 
-/// Using regex, extract outage information from the text extracted from image
-pub fn parse_text(text: &str) -> Vec<Outage> {
+/// Using regex, extract interruption information from the text extracted from image
+pub fn parse_text(text: &str) -> Vec<Interruption> {
+    // let re: Regex = Regex::new(r"(?mi)^(?P<region>[a-z\s]*\sregion)?\s*((parts\sof)?\b(?P<county>[a-z\s]*\scounty\b))?\s*(\barea:?)\s\b(?P<area>[a-z\s,\.]*)(\bdate:?)\s\b(?P<day>[a-z]*)\b\s*(?P<date>[\d\.]*)\b\s(\btime:?)\s\b(?P<start>[\d\.]*)\b\s\b(?P<start_period>[ap]\.m\.)\s*[-~—]\s*\b(?P<end>[\d\.]*)\s*(?P<end_period>[ap]\.m\.)\s*(?P<locations>[a-z0-9&,\s\.]*)\n")
     let re: Regex = Regex::new(r"(?mi)^(?P<region>[a-z\s]*\sregion)?\s*((parts\sof)?\b(?P<county>[a-z\s]*\scounty\b))?\s*(\barea:?)\s\b(?P<area>[a-z\s,\.]*)(\bdate:?)\s\b(?P<day>[a-z]*)\b\s*(?P<date>[\d\.]*)\b\s(\btime:?)\s\b(?P<start>[\d\.]*)\b\s\b(?P<start_period>[ap]\.m\.)\s*[-~—]\s*\b(?P<end>[\d\.]*)\s*(?P<end_period>[ap]\.m\.)\s*(?P<locations>[a-z0-9&,\s\.-]*)\n")
         .unwrap();
-    let mut outages: Vec<Outage> = Vec::new();
+    let mut interruptions: Vec<Interruption> = Vec::new();
 
     for captures in re.captures_iter(text) {
-        let mut outage = Outage::default();
+        let mut interruption = Interruption::default();
 
         if let Some(n) = captures.name("region") {
-            outage.region = Some(n.as_str().to_string());
+            interruption.region = Some(n.as_str().to_string());
         }
 
         if let Some(n) = captures.name("county") {
-            outage.county = Some(n.as_str().to_string());
+            interruption.county = Some(n.as_str().to_string());
         }
 
         if let Some(n) = captures.name("area") {
-            outage.area = Some(n.as_str().to_string());
+            interruption.area = Some(n.as_str().to_string());
         }
 
         if let Some(n) = captures.name("day") {
             if let Some(d) = captures.name("date") {
                 let d_str = format!("{} {}", n.as_str(), d.as_str());
-                outage.date = Some(d_str);
+                interruption.date = Some(d_str);
             } else {
-                outage.date = Some(n.as_str().to_string());
+                interruption.date = Some(n.as_str().to_string());
             }
         }
 
         if let Some(n) = captures.name("start") {
             if let Some(t) = captures.name("start_period") {
                 let t_str = format!("{} {}", n.as_str(), t.as_str());
-                outage.start_time = Some(t_str);
+                interruption.start_time = Some(t_str);
             } else {
-                outage.start_time = Some(n.as_str().to_string());
+                interruption.start_time = Some(n.as_str().to_string());
             }
         }
 
         if let Some(n) = captures.name("end") {
             if let Some(t) = captures.name("end_period") {
                 let t_str = format!("{} {}", n.as_str(), t.as_str());
-                outage.end_time = Some(t_str);
+                interruption.end_time = Some(t_str);
             } else {
-                outage.end_time = Some(n.as_str().to_string());
+                interruption.end_time = Some(n.as_str().to_string());
             }
         }
 
         if let Some(n) = captures.name("locations") {
-            outage.locations = Some(n.as_str().to_string());
+            interruption.locations = Some(n.as_str().to_string());
         }
 
-        outages.push(outage);
+        interruptions.push(interruption);
     }
 
-    outages
+    interruptions
 }
 
-/// Go through all texts from all the outage images and return the first date found
-pub fn extract_date(outage_texts: Vec<String>) -> String {
-    for outage_text in outage_texts {
-        let outages = parse_text(&outage_text);
-        for outage in outages {
-            if let Some(date) = outage.date {
+/// Go through all texts from all the interruption images and return the first date found
+pub fn extract_date(interruption_texts: Vec<String>) -> String {
+    for interruption_text in interruption_texts {
+        let interruptions = parse_text(&interruption_text);
+        for interruption in interruptions {
+            if let Some(date) = interruption.date {
                 return date;
             }
         }
