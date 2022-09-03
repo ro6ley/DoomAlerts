e3ee5ac680920acc9bc7d2bfec4c14df293173ff
Robley G <robleyadrian@gmail.com>
2022-09-03 19:00:14 +0300

    use tokio interval

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,45 @@
 use log::{error, info};
 use std::{collections::HashMap, env};
+use tokio::time::Duration;
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
 
     let watchlist: String = env::var("WATCHLIST").expect("$WATCHLIST env var is not set");
+    let interval: u64 = env::var("INTERVAL")
+        .expect("$INTERVAL env var is not set")
+        .parse::<u64>()
+        .expect("$INTERVAL env var value is NOT a number!");
 
-    let interruptions: HashMap<u64, Vec<String>> =
-        doom_alerts::tweets::fetch_tweets("KenyaPower_Care").await;
+    let mut interval_timer = tokio::time::interval(Duration::from_secs(interval));
+    loop {
+        interval_timer.tick().await;
 
-    for (id, interruption_texts) in &interruptions {
-        let interruption_date: String =
-            doom_alerts::interruption::extract_date(interruption_texts.clone())
-                .unwrap_or_else(|| "N/A".to_string());
-        let affected: bool =
-            doom_alerts::search::search(interruption_texts.clone(), watchlist.clone())
-                .unwrap_or(false);
+        info!("Fetching tweets every {interval} seconds ... ");
 
-        if affected {
-            info!("One or more areas in watchlist will be affected by a scheduled power supply interruption. Sending interruption information via email... ");
-            let tweet_link: String = doom_alerts::tweets::build_tweet_link(*id, "KenyaPower_Care");
+        let interruptions: HashMap<u64, Vec<String>> =
+            doom_alerts::tweets::fetch_tweets("KenyaPower_Care").await;
 
-            // TODO: send email once
-            match doom_alerts::notifications::send_email(interruption_date, tweet_link).await {
-                Ok(msg) => info!("{msg}"),
-                _ => error!("Email was not sent."),
-            };
+        for (id, interruption_texts) in &interruptions {
+            let interruption_date: String =
+                doom_alerts::interruption::extract_date(interruption_texts.clone())
+                    .unwrap_or_else(|| "N/A".to_string());
+            let affected: bool =
+                doom_alerts::search::search(interruption_texts.clone(), watchlist.clone())
+                    .unwrap_or(false);
+
+            if affected {
+                info!("One or more areas in watchlist will be affected by a scheduled power supply interruption. Sending interruption information via email... ");
+                let tweet_link: String =
+                    doom_alerts::tweets::build_tweet_link(*id, "KenyaPower_Care");
+
+                // TODO: send email once
+                match doom_alerts::notifications::send_email(interruption_date, tweet_link).await {
+                    Ok(msg) => info!("{msg}"),
+                    _ => error!("Email was not sent."),
+                };
+            }
         }
     }
 }
